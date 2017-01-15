44db9fa28d2997dfbad522914b0e193e965a345d
wenbo <thierryhenrytracy@163.com>
2017-01-14 22:31:46 -0500

    make a directory for verifiers.

--- Joiner/PolynomialJoiner.h
+++ Joiner/PolynomialJoiner.h
@@ -30,7 +30,7 @@ private:
 	unordered_map<t_rule, int, rule_hash> best_rule_count;
 
 	//SIGMOD 13 - similarity
-	double get_similarity(int, int);
+	double sigmod13_get_similarity(int, int);
 	double rule_gain(t_rule, umpsi, int);
 
 	//Sig builders
