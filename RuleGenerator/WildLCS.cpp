9563fbc20b52b3d249a6cd3e9158d2d75cec91c1
wenbo <thierryhenrytracy@163.com>
2017-01-26 11:03:29 -0500

    the genuine wild lcs.

--- RuleGenerator/WildLCS.cpp
+++ RuleGenerator/WildLCS.cpp
@@ -42,65 +42,63 @@ vector<t_rule> WildLCS::gen_rules()
 
 	for (int i = 0; i < n; i ++)
 	{
-		for (string cur_string : tokens[i])
-		{
-			vector<int> token_id;
-/*
-			for (int j = 0; j < (int) tokens[i].size(); j ++)
-				for (int k = 0; k < (int) tokens[i][j].size(); k ++)
-				{
-					cur_string += tokens[i][j][k];
-					token_id.push_back(j);
-				}
-*/
-			for (auto j = 0; j < cur_string.size(); j ++)
-				token_id.push_back(1);
+		string cur_string = "";
+		vector<int> token_id;
 
-			//traversal
-			H.clear();
-			for (auto j = 0; j < trie.size(); j ++)
-				used[j].clear();
-			H.emplace_back(0, make_pair(0, 0));
-			used[0].insert(make_pair(0, 0));
-			for (auto j = 0; j < cur_string.size(); j ++)
+		for (int j = 0; j < (int) tokens[i].size(); j ++)
+			for (int k = 0; k < (int) tokens[i][j].size(); k ++)
 			{
-				vector<pair<int, pair<int, int>>> cur_H = H;
-				for (auto cp : cur_H)
-				{
-					int node = cp.first;
-					int st = cp.second.first;
-					int en = cp.second.second;
-
-					//include current character
-					if (trie[node].count(cur_string[j]))
-					{
-						int new_node = trie[node][cur_string[j]];
-						int new_st = (st == 0 ? token_id[j] : st);
-						int new_en = token_id[j];
-						if (! used[new_node].count(make_pair(new_st, new_en)))
-						{
-							H.emplace_back(new_node, make_pair(new_st, new_en));
-							used[new_node].insert(make_pair(new_st, new_en));
-						}
-					}
-				}
+				cur_string += tokens[i][j][k];
+				token_id.push_back(j);
 			}
 
-			//output rules
-			for (auto cp : H)
+//		for (auto j = 0; j < cur_string.size(); j ++)
+//			token_id.push_back(1);
+
+		//traversal
+		H.clear();
+		for (auto j = 0; j < trie.size(); j ++)
+			used[j].clear();
+		H.emplace_back(0, make_pair(0, 0));
+		used[0].insert(make_pair(0, 0));
+		for (auto j = 0; j < cur_string.size(); j ++)
+		{
+			vector<pair<int, pair<int, int>>> cur_H = H;
+			for (auto cp : cur_H)
 			{
 				int node = cp.first;
 				int st = cp.second.first;
 				int en = cp.second.second;
-				if (! contain_word[node].size())
-					continue;
-				vector<string> lhs, rhs;
-				lhs.push_back(contain_word[node]);
-//			for (int j = st - 1; j < en; j ++)
-//				rhs.push_back(tokens[i][j]);
-				rhs.push_back(cur_string);
-				rules.insert(make_pair(lhs, rhs));
+
+				//include current character
+				if (trie[node].count(cur_string[j]))
+				{
+					int new_node = trie[node][cur_string[j]];
+					int new_st = (st == 0 ? token_id[j] + 1 : st);
+					int new_en = token_id[j] + 1;
+					if (! used[new_node].count(make_pair(new_st, new_en)))
+					{
+						H.emplace_back(new_node, make_pair(new_st, new_en));
+						used[new_node].insert(make_pair(new_st, new_en));
+					}
+				}
 			}
+		}
+
+		//output rules
+		for (auto cp : H)
+		{
+			int node = cp.first;
+			int st = cp.second.first;
+			int en = cp.second.second;
+			if (! contain_word[node].size())
+				continue;
+			vector<string> lhs, rhs;
+			lhs.push_back(contain_word[node]);
+			for (int j = st - 1; j < en; j ++)
+				rhs.push_back(tokens[i][j]);
+//			rhs.push_back(cur_string);
+			rules.insert(make_pair(lhs, rhs));
 		}
 	}
 
